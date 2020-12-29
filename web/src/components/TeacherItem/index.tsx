5f79afd2487f307afdfbfee39ae12a0f48b6a974
lorenatoscano <lorenatoscano@ufrn.edu.br>
2020-12-29 17:47:24 -0300

    Integra listagem de professores a api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,29 +3,36 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
         <header>
-          <img src="https://image.flaticon.com/icons/svg/194/194938.svg" alt="Fulana de Tal"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Fulana de Tal</strong>
-            <span>Química</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
 
-        <p>
-          Entusiasta das melhores tecnologias de química avançada.
-          <br /><br />
-          Apaixonada por explodir coisas em laboratório e por mudar a vida das
-          pessoas através das experiências. Mais de 200.000 pessoas já passaram
-          por uma das minhas explosões.
-        </p>
+        <p>{teacher.bio}</p>
 
         <footer>
           <p>
             Preço/hora
-            <strong>R$ 50,00</strong>
+            <strong>R$ {teacher.cost}</strong>
           </p>
           <button type="button">
             <img src={whatsappIcon} alt="Whatsapp" />
